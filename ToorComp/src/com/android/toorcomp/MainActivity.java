b7554a0a8025797aa26735d5b729de02c747a640
Mrspyros <Mrspyros@gmail.com>
2015-02-27 14:37:11 +0200

    Moved intialization to MainActivity Moved all messages to onPostExecute   Now Async task works fine   Spinner OK
    Changed compass on map

--- ToorComp/src/com/android/toorcomp/MainActivity.java
+++ ToorComp/src/com/android/toorcomp/MainActivity.java
@@ -1,16 +1,8 @@
 package com.android.toorcomp;
 
-import java.util.concurrent.ExecutionException;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Toast;
 
 /**
  * @author mrspyros
@@ -18,154 +10,29 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity {
 
-	private boolean download = false;
-	private ProgressDialog pd;
-	private Context context;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.main);
-
-		final Startup init = new Startup(this);
-
-		Boolean XMLExists = init.CheckXML();
-
+		
+		
+		//------ Check ic XML Exists and notify globals
+		
 		final Intent intent = new Intent(getApplicationContext(),
 				MainScreen.class);
+		final Startup init = new Startup(this);
+		Globals g = Globals.getInstance();
+		Boolean XMLExists = init.CheckXML();
 
 		if (!XMLExists) {
-
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-			builder.setTitle(R.string.noxmltitle);
-			builder.setMessage(R.string.noxmlmessage);
-
-			builder.setPositiveButton("YES",
-					new DialogInterface.OnClickListener() {
-
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-
-							// Code that is executed when clicking YES
-							//dialog.dismiss();
-						
-							AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
-								
-								@Override
-								protected void onPreExecute() {
-								
-									try {
-										pd = new ProgressDialog(MainActivity.this);
-										pd.setTitle("Processing...");
-										pd.setMessage("Please wait.");
-										pd.setCancelable(false);
-										pd.setIndeterminate(true);
-										pd.show();
-									} catch (Exception e) {
-										Toast.makeText(MainActivity.this,"Error= "+e,
-												Toast.LENGTH_LONG).show();
-										
-										// TODO Auto-generated catch block
-										//e.printStackTrace();
-									}
-								
-								}
-									
-								@Override
-								protected Void doInBackground(Void... arg0) {
-									try {
-										download = init.DownloadXML();
-										Thread.sleep(5000);
-									} catch (InterruptedException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-									return null;
-								}
-								
-								@Override
-								protected void onPostExecute(Void result) {
-																	
-									if (pd!=null) {
-									pd.dismiss();
-									//	b.setEnabled(true);
-									}
-								}
-									
-							};
-							try {
-								Void str_result= task.execute().get();
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (ExecutionException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							//task.execute((Void[])null);
-							
-						
-							
-						//	download = init.DownloadXML();
-
-							if (!download) {
-								Globals g = Globals.getInstance();
-								Toast.makeText(getApplicationContext(),
-										R.string.noxmldl + g.getWait(),
-										Toast.LENGTH_LONG).show();
-								g.setXmLERROR("YES");
-								dialog.dismiss();
-								finish();
-								startActivity(intent);
-							} else {
-								Globals g = Globals.getInstance();
-								Toast.makeText(getApplicationContext(),
-										R.string.noxmldlok, Toast.LENGTH_LONG)
-										.show();
-								g.setXmLERROR("NO");
-
-								dialog.dismiss();
-								finish();
-								startActivity(intent);
-
-							}
-
-						}
-
-					});
-
-			builder.setNegativeButton("NO",
-					new DialogInterface.OnClickListener() {
-
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-
-							// Code that is executed when clicking NO
-							Globals g = Globals.getInstance();
-							g.setXmLERROR("YES");
-							dialog.dismiss();
-							finish();
-							startActivity(intent);
-
-						}
-
-					});
-
-			AlertDialog alert = builder.create();
-			alert.show();
-
-		}
-
-		else {
-			Globals g = Globals.getInstance();
-			g.setXmLERROR("NO");
-			finish();
-			startActivity(intent);
-		}
+			g.setXmLERROR(true);
+			g.set_Xml_Download_Answer("YES");
+		} else
+			g.setXmLERROR(false);
+		finish();
+		startActivity(intent);
 
 	}
 
-	
-	
 }
--- ToorComp/src/com/android/toorcomp/MainScreen.java
+++ ToorComp/src/com/android/toorcomp/MainScreen.java
@@ -3,36 +3,25 @@ package com.android.toorcomp;
 import java.io.File;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainScreen extends Activity {
 
+	private boolean download = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		// Globals g = Globals.getInstance();
-		String ERROR = Globals.getInstance().getXmLERROR();
-
-		// if (ERROR=="YES") Toast.makeText(getApplicationContext(),
-		// "Δέν βρέθηκαν αποθηκευμένα σημεία ενδιαφέροντος",
-		// Toast.LENGTH_LONG).show();
-
-		// Toast.makeText(getApplicationContext(),"message="+ ERROR,
-		// Toast.LENGTH_LONG).show();
-		// }
 
 		File M_PATH = new File(Environment.getExternalStorageDirectory(),
 				"osmdroid");
@@ -52,10 +41,9 @@ public class MainScreen extends Activity {
 		Button btn2 = (Button) findViewById(R.id.btn2);
 		btn2.setOnClickListener(new android.view.View.OnClickListener() {
 			public void onClick(View v) {
-				 Intent intent =new Intent(getApplicationContext(),
-				 Pois.class);//Pois.class);
-				//Intent intent = new Intent(getApplicationContext(),
-				//		webview.class);
+				Intent intent = new Intent(getApplicationContext(), Pois.class);// Pois.class);
+				// Intent intent = new Intent(getApplicationContext(),
+				// webview.class);
 				startActivity(intent);
 			}
 		});
@@ -82,58 +70,167 @@ public class MainScreen extends Activity {
 		Button qRcode = (Button) findViewById(R.id.qrcode);
 		qRcode.setOnClickListener(new android.view.View.OnClickListener() {
 			public void onClick(View v) {
-		  
+
 				try {
 
 					String packageString = "com.android.toorcomp";
-					Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+					Intent intent = new Intent(
+							"com.google.zxing.client.android.SCAN");
 					intent.setPackage(packageString);
 					intent.putExtra("SCAN_MODE", "SCAN_MODE");
 					startActivityForResult(intent, 0);
-					
-					
-					/*
-					Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-	                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-	                startActivityForResult(intent, 0);
-	            */
 
 				} catch (Exception e) {
 
-					Toast.makeText(getApplicationContext(),
-							e.toString(),
-							 Toast.LENGTH_LONG).show();
+					Toast.makeText(getApplicationContext(), e.toString(),
+							Toast.LENGTH_LONG).show();
 
 				}
-				
-	        }
 
-		
+			}
+
 		});
 
+		final Startup init = new Startup(this);
+
+		Globals g = Globals.getInstance();
+
+		boolean XMLExists = !g.getXmLERROR();
+		String XMLanswer = g.get_Xml_Download_Answer();
+
+		if (!XMLExists && XMLanswer == "YES") {
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+			builder.setTitle(R.string.noxmltitle);
+			builder.setMessage(R.string.noxmlmessage);
+
+			builder.setPositiveButton("YES",
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(final DialogInterface dialog,
+								int which) {
+
+							// Code that is executed when clicking YES
+							// dialog.dismiss();
+
+							AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+								ProgressDialog pd = new ProgressDialog(
+										MainScreen.this);
+
+								@Override
+								protected void onPreExecute() {
+
+									try {
+
+										pd.setTitle("Downloading");
+										pd.setMessage("Παρακαλώ περιμένετε.");
+										pd.setCancelable(true);
+										pd.setIndeterminate(true);
+										pd.show();
+									} catch (Exception e) {
+										Toast.makeText(getApplicationContext(),
+												"1= " + e.toString(),
+												Toast.LENGTH_LONG).show();
+
+									}
+
+								}
+
+								@Override
+								protected Void doInBackground(Void... arg0) {
+									try {
+										download = init.DownloadXML();
+										Thread.sleep(5000);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									return null;
+								}
+
+								@Override
+								protected void onPostExecute(Void result) {
+
+									try {
+										if (pd.isShowing()) {
+											pd.hide();
+											pd.dismiss();
+										}
+									} catch (Exception e) {
+										Toast.makeText(getApplicationContext(),
+												"2= " + e.toString(),
+												Toast.LENGTH_LONG).show();
+									}
+
+									if (!download) {
+										Globals g = Globals.getInstance();
+										Toast.makeText(getApplicationContext(),
+												R.string.noxmldl + g.getWait(),
+												Toast.LENGTH_LONG).show();
+										g.setXmLERROR(true);
+										g.set_Xml_Download_Answer("NO");
+										dialog.dismiss();
+
+									} else {
+										Globals g = Globals.getInstance();
+										Toast.makeText(getApplicationContext(),
+												R.string.noxmldlok,
+												Toast.LENGTH_LONG).show();
+										g.setXmLERROR(false);
+										dialog.dismiss();
+
+									}
+
+								}
+
+							};
+							task.execute((Void[]) null);
+
+						}
+
+					});
+
+			builder.setNegativeButton("NO",
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+
+							// Code that is executed when clicking NO
+							Globals g = Globals.getInstance();
+							g.setXmLERROR(true);
+							g.set_Xml_Download_Answer("NO");
+							dialog.dismiss();
+
+						}
+
+					});
+
+			AlertDialog alert = builder.create();
+			alert.show();
+
+		}
+
 	}
-	
+
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {           
-	    super.onActivityResult(requestCode, resultCode, data);
-	    if (requestCode == 0) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == 0) {
 
-	        if (resultCode == RESULT_OK) {
-	        	Globals g = Globals.getInstance();
-	        	g.setWebViewUrl(data.getStringExtra("SCAN_RESULT"));
-	        	Intent intent = new Intent(getApplicationContext(), webview.class);
+			if (resultCode == RESULT_OK) {
+				Globals g = Globals.getInstance();
+				g.setWebViewUrl(data.getStringExtra("SCAN_RESULT"));
+				Intent intent = new Intent(getApplicationContext(),
+						webview.class);
 				startActivity(intent);
-	        }
-	        if(resultCode == RESULT_CANCELED){
-	            //handle cancel
-	        }
-	    }
+			}
+			if (resultCode == RESULT_CANCELED) {
+				// handle cancel
+			}
+		}
 	}
-	
-	
-	
-	
-	
+
 }
-
-
